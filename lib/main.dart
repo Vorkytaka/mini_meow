ac4dfc606c6bd9e55f26c2d4bab59bd52ff1a69b
Vorkytaka <vorkytaka@gmail.com>
2024-06-16 23:13:11 +0300

    - Add background

--- lib/main.dart
+++ lib/main.dart
@@ -3,5 +3,22 @@ import 'package:flutter/material.dart';
 import 'package:mini_meow/game/game.dart';
 
 void main() {
-  runApp(GameWidget(game: MeowGame()));
+  runApp(
+    GameWidget(
+      game: MeowGame(),
+      backgroundBuilder: (context) {
+        return const SizedBox.expand(
+          child: DecoratedBox(
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage('assets/background/woods.jpg'),
+                fit: BoxFit.cover,
+                repeat: ImageRepeat.repeat,
+              ),
+            ),
+          ),
+        );
+      },
+    ),
+  );
 }

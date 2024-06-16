0ef5622321394411537bf8baad1736edf6a9d4b4
Vorkytaka <vorkytaka@gmail.com>
2024-06-16 21:59:51 +0300

    - Rotate mouse

--- lib/game/target.dart
+++ lib/game/target.dart
@@ -1,4 +1,5 @@
 import 'dart:async';
+import 'dart:math';
 
 import 'package:flame/components.dart';
 import 'package:flame/effects.dart';
@@ -11,13 +12,15 @@ class Target extends SpriteComponent
   Target()
       : super(
           anchor: Anchor.center,
-          size: Vector2(47, 99.5),
+          size: Vector2(99.5, 47),
+          // angle: 1,
         );
 
-  final _controller = EffectController(
+  final _moveController = EffectController(
     speed: 100,
     curve: Curves.easeInOut,
   );
+  final _rotateController = EffectController(duration: 1);
 
   @override
   FutureOr<void> onLoad() async {
@@ -36,13 +39,27 @@ class Target extends SpriteComponent
   }
 
   void _moveTo() {
-    _controller.setToStart();
+    _moveController.setToStart();
+    _rotateController.setToStart();
     final moveTo = game.area.randomPoint();
     add(
       MoveToEffect(
         moveTo,
-        _controller,
+        _moveController,
         onComplete: _moveTo,
+      ),
+    );
+
+    // TODO: Подумой
+    // Calculate the angle
+    final deltaX = moveTo.x - position.x;
+    final deltaY = moveTo.y - position.y;
+    final angle = atan2(deltaY, deltaX);
+
+    add(
+      RotateEffect.to(
+        angle,
+        _rotateController,
       ),
     );
   }
